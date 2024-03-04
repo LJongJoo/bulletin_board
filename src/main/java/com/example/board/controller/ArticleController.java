88c81ff87e8ad2742703d5b0f4ed56ecce149eb6
Cat Hanbit <tlzhsh2@dgu.ac.kr>
2024-03-04 18:36:55 +0900

    show.mustache 오류 수정 및 새글을 작성했을 때 해당 페이지로 이동기능 생성

--- src/main/java/com/example/board/controller/ArticleController.java
+++ src/main/java/com/example/board/controller/ArticleController.java
@@ -6,6 +6,7 @@ import com.example.board.entity.Article;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,14 +31,14 @@ public class ArticleController {
         log.info(articleEntity.toString());
         Article saved = articleRepository.save(articleEntity);
         log.info(saved.toString());
-        return "";
+        return "redirect:/articles/"+saved.getId();
     }
 
     @GetMapping("/articles/{id}")
-    public String show(@PathVariable Long id) {
+    public String show(@PathVariable Long id, Model model) {
         Article articleEntity = articleRepository.findById(id).orElse(null);
-
-        return "";
+        model.addAttribute("article",articleEntity);
+        return "articles/show";
     }
 
 }
